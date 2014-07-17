20fa018dacb388600fe5e63aae7207b25ae26437
Alexander Kedrik <alkedr@yandex-team.ru>
2014-07-17 21:07:40 +0400

    WIP

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QtWidgets>
 #include <QtNetwork>
 #include <iostream>
-#include <unistd.h>
+#include <boost/thread/future.hpp>
 
 
 static QByteArray makeScreenshot() {
@@ -29,100 +29,43 @@ static QNetworkRequest publishRequest(QString fileName) {
 
 
 
-template<class Callback> void retrieveUploadUrl(QNetworkAccessManager * qnam, QString fileName, Callback callback) {
-	QNetworkReply * pathResponse = qnam->get(pathRequest(fileName));
+QNetworkAccessManager qnam;
+
+
+// static void retrieveUploadUrl
+
+
+static void uploadFile(QString name, QString url, QByteArray contents) {
+	QNetworkReply * uploadResponse = qnam.put(request(url), screenshotByteArray);
+}
+
+
+static void retrieveUploadUrl(QString name, QByteArray contents) {
+	QNetworkReply * pathResponse = qnam.get(pathRequest("123.png"));
 	QObject::connect(pathResponse, &QNetworkReply::finished, 
 		[&]() {
 			auto json = QJsonDocument::fromJson(pathResponse->readAll()).object();
-			// std::cout << json.toJson() << std::endl;
-			std::cout << "Урл для загрузки получен: " << json["href"].toString().toStdString() << std::endl;
-			callback(json["href"].toString());
+			std::cout << json.toJson() << std::endl;
+			std::cout << "Урл для загрузки получен: " << json["href"].toString() << std::endl;
+			uploadFile(name, json["href"].toString(), contents);
 		}
 	);
 }
-
-template<class Callback> void upload(QNetworkAccessManager * qnam, QString url, QByteArray contents, Callback callback) {
-	QNetworkReply * uploadResponse = qnam->put(request(url), contents);
-	QObject::connect(uploadResponse, &QNetworkReply::finished,
-		[&]() {
-			std::cout << "Файл загружен" << std::endl;
-			callback();
-		}
-	);
-}
-
-template<class Callback> void publish(QNetworkAccessManager * qnam, QString fileName, Callback callback) {
-	// QNetworkReply * uploadResponse = qnam.put(url), contents);
-	// QObject::connect(uploadResponse, &QNetworkReply::finished,
-	// 	[&]() {
-	// 		std::cout << "Файл загружен" << std::endl;
-	// 		callback();
-	// 	}
-	// );
-
-
-	auto body = new QByteArray("<propertyupdate xmlns=\"DAV:\"><set><prop><public_url xmlns=\"urn:yandex:disk:meta\">true</public_url></prop></set></propertyupdate>");
-	auto buffer = new QBuffer(body);
-	buffer->open(QIODevice::ReadOnly);
-	QNetworkReply * publishResponse = qnam->sendCustomRequest(publishRequest(fileName), "PROPPATCH", buffer);
-	QObject::connect(publishResponse, &QNetworkReply::finished, 
-		[&]() {
-			std::cout << "Файл расшарен" << std::endl;
-			std::cout << QString(publishResponse->readAll()).toStdString() << std::endl;
-			callback("");
-		}
-	);
-}
-
-
 
 
 int main(int argc, char ** argv) {
 	QApplication app(argc, argv);
 	QApplication::setFont(QFont("Arial", 20));
 
+	QByteArray screenshotByteArray = makeScreenshot();
 
+	// auto retrieveUploadUrl = [](QString fileName) {
 
-
-
-	auto qnam = new QNetworkAccessManager();
-
-
-	// static void retrieveUploadUrl
-
-
-	// static void uploadFile(QString name, QString url, QByteArray contents) {
-	// 	QNetworkReply * uploadResponse = qnam.put(request(url), contents);
 	// }
 
 
 
 
-
-	QByteArray screenshotByteArray = makeScreenshot();
-	QString fileName = "123.png";
-
-	retrieveUploadUrl(qnam, fileName, 
-		[&](QString url) {
-			upload(qnam, url, screenshotByteArray,
-				[&]() {
-					publish(qnam, fileName, 
-						[&](QString url) {
-							// QApplication::clipboard()->setText();
-							// QClipboard *clipboard = QApplication::clipboard();
-							std::cout << "finished" << std::endl;
-						}
-					);
-				}
-			); 
-		}
-	);
-
-
-
-
-
-/*
 	QNetworkReply * pathResponse = qnam.get(pathRequest("123.png"));
 	QObject::connect(pathResponse, &QNetworkReply::finished, 
 		[&]() {
@@ -153,6 +96,6 @@ int main(int argc, char ** argv) {
 			);
 		}
 	);
-*/
+
 	return app.exec();
 }

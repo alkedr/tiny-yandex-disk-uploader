cc500f721351c76c546a63fb44efba822639aeac
Alexander Kedrik <alkedr@yandex-team.ru>
2014-07-18 20:32:19 +0400

    IT WORKS!

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SHELL := bash
 
 tiny-yandex-disk-uploader: main.c Makefile
-	clang -std=c11 -O0 -ggdb3 -Weverything -Wno-c++98-compat -lssl -lcrypto -lcairo -lX11 $< -o $@
+	clang -std=c11 -O0 -ggdb3 -Weverything -Wno-c++98-compat -Wno-documentation -Wno-padded  `pkg-config --cflags --libs gtk+-3.0` -lssl -lcrypto -lcairo -lX11 $< -o $@
 
 #upx --best --ultra-brute --lzma -qqq $@
 
--- main.c
+++ main.c
@@ -15,20 +15,22 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
+// #include <gtk/gtk.h>
 
-static void log(const char * s) {
-  static long long startTime = 0;
 
-  struct timeval currentTimeval;
-  gettimeofday(&currentTimeval, NULL);
-  long long currentTime = currentTimeval.tv_sec * 1000 + currentTimeval.tv_usec / 1000;
-  if (startTime) {
-    printf("%lldms\n", currentTime - startTime);
-  }
-  printf("%s  ", s);
-  fflush(stdout);
-  startTime = currentTime;
-}
+// static void log(const char * s) {
+//   static long long startTime = 0;
+
+//   struct timeval currentTimeval;
+//   gettimeofday(&currentTimeval, NULL);
+//   long long currentTime = currentTimeval.tv_sec * 1000 + currentTimeval.tv_usec / 1000;
+//   if (startTime) {
+//     printf("%lldms\n", currentTime - startTime);
+//   }
+//   printf("%s  ", s);
+//   fflush(stdout);
+//   startTime = currentTime;
+// }
 
 
 struct Screenshot {
@@ -96,10 +98,49 @@ static SSL * sslConnect(SSL_CTX * sslContext, const char * hostName, uint16_t po
 #define OAUTH_TOKEN "45e24fd66c884bafaae7cc4e2e462789"
 
 
+int main(int argc, char *argv[]) {
+  // gtk_init(&argc, &argv);
 
-const void writePutEmptyFileRequest(SSL * handle, const char * fileName) {
+  // GtkWidget * window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  // gtk_window_set_title(GTK_WINDOW(window), "Window");
+  // GtkWidget * label = gtk_label_new("ddsg");
+  // gtk_container_add(GTK_CONTAINER(window), label);
+  // g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  // gtk_widget_show(window);
+  // gtk_widget_show(label);
+
+
+
+
+  SSL_load_error_strings();
+  SSL_library_init();
+  SSL_CTX * sslContext = SSL_CTX_new(SSLv23_client_method());
+  if (sslContext == NULL) { ERR_print_errors_fp(stderr); return 1; }
+
+
+  static char fileName[128] = {0};
+  time_t t = time(NULL);
+  strftime(fileName, sizeof(fileName), "Screenshots/Screenshot-alkedr_%Y.%m.%d_%H.%M.%S.png", localtime(&t));
+
+  // printf("Uploading screenshot %s\n", fileName);
+
+
+
+  // log("Connecting");
+  SSL * handle = sslConnect(sslContext, "webdav.yandex.com", 443);
+  if (handle == NULL) { return 1; }
+
+
+  // log("Taking screenshot");
+  void * scr = malloc(1024*1024);
+  int screenshotSize;
+  takeScreenshot(scr, &screenshotSize);
+
+
   static char request[1024*1024];
-  int requestLength = sprintf(request,
+
+  // log("Sending request to create empty file");
+  SSL_write(handle, request, sprintf(request,
     "PUT /%s HTTP/1.1\r\n"
     "Host: webdav.yandex.ru\r\n"
     "Accept: */*\r\n"
@@ -111,13 +152,10 @@ const void writePutEmptyFileRequest(SSL * handle, const char * fileName) {
     "Content-Length: 0\r\n"
     "\r\n",
     fileName
-  );
-  SSL_write(handle, request, requestLength);
-}
+  ));
 
-const void writePublishFileRequest(SSL * handle, const char * fileName) {
-  static char buffer[1024*1024];
-  int requestLength = sprintf(buffer,
+  // log("Sending request to publish file");
+  SSL_write(handle, request, sprintf(request,
     "PROPPATCH /%s HTTP/1.1\r\n"
     "Host: webdav.yandex.ru\r\n"
     "Authorization: OAuth " OAUTH_TOKEN "\r\n"
@@ -126,13 +164,10 @@ const void writePublishFileRequest(SSL * handle, const char * fileName) {
     "<propertyupdate xmlns=\"DAV:\"><set><prop><public_url xmlns=\"urn:yandex:disk:meta\">true</public_url></prop></set></propertyupdate>"
     "\r\n",
     fileName
-  );
-  SSL_write(handle, buffer, requestLength);
-}
+  ));
 
-const void writePutFileRequest(SSL * handle, const char * fileName, void * contents, int size) {
-  static char buffer[1024*1024];
-  int requestLength = sprintf(buffer,
+  // log("Sending request to replace empty file with real one");
+  SSL_write(handle, request, sprintf(request,
     "PUT /%s HTTP/1.1\r\n"
     "Host: webdav.yandex.ru\r\n"
     "Accept: */*\r\n"
@@ -142,319 +177,46 @@ const void writePutFileRequest(SSL * handle, const char * fileName, void * conte
     "Content-Length: %d\r\n"
     "\r\n",
     fileName,
-    size
-  );
-  SSL_write(handle, buffer, requestLength);
-  SSL_write(handle, contents, size);
-}
+    screenshotSize
+  ));
+  SSL_write(handle, scr, screenshotSize);
 
 
+  // writePutFileRequest(handle, fileName, scr, screenshotSize);
 
-
-
-int main() {
-  const char * fileName = "8.png";
-
-  SSL_load_error_strings();
-  SSL_library_init();
-  SSL_CTX * sslContext = SSL_CTX_new(SSLv23_client_method());
-  if (sslContext == NULL) { ERR_print_errors_fp(stderr); return 1; }
-
-  log("Connecting");
-  SSL * handle = sslConnect(sslContext, "webdav.yandex.com", 443);
-  if (handle == NULL) { return 1; }
-
-
-  log("Taking screenshot");
-  void * scr = malloc(1024*1024);
-  int screenshotSize;
-  takeScreenshot(scr, &screenshotSize);
-
-  log("Sending requests");
-  writePutEmptyFileRequest(handle, fileName);
-
-  writePublishFileRequest(handle, fileName);
-
-  writePutFileRequest(handle, fileName, scr, screenshotSize);
-
-  log("Receiving response");
+  // log("Searching for a link");
   static char response[10240];
 
   SSL_read(handle, response, sizeof(response));
-  printf("\n\n%s", response);
-  memset(response, 0, sizeof(response));
-
   SSL_read(handle, response, sizeof(response));
-  printf("\n\n%s", response);
-  memset(response, 0, sizeof(response));
-
   SSL_read(handle, response, sizeof(response));
-  printf("\n\n%s", response);
   memset(response, 0, sizeof(response));
-
   SSL_read(handle, response, sizeof(response));
-  printf("\n\n%s", response);
-  memset(response, 0, sizeof(response));
-
-  log("Receiving response 2");
-  SSL_read(handle, response, sizeof(response));
-  printf("\n\n%s", response);
-  memset(response, 0, sizeof(response));
-
-  SSL_read(handle, response, sizeof(response));
-  printf("\n\n%s", response);
-  memset(response, 0, sizeof(response));
-
-
-
-  // static char request[1024*1024];
-  // int requestLength = 0;
-  // requestLength += sprintf(request + requestLength,
-    // "PUT /%s HTTP/1.1\r\n"
-    // "Host: webdav.yandex.ru\r\n"
-    // "Accept: */*\r\n"
-    // "Authorization: OAuth " OAUTH_TOKEN "\r\n"
-    // "Content-Type: image/png\r\n"
-    // "Content-Length: 0\r\n"
-    // "Etag: d41d8cd98f00b204e9800998ecf8427e\r\n"
-    // "Sha256: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\r\n"
-    // "\r\n"
-    // "PROPPATCH /%s HTTP/1.1\r\n"
-    // "Host: webdav.yandex.ru\r\n"
-    // "Authorization: OAuth " OAUTH_TOKEN "\r\n"
-    // "Content-Length: 128\r\n"
-    // "\r\n"
-    // "<propertyupdate xmlns=\"DAV:\"><set><prop><public_url xmlns=\"urn:yandex:disk:meta\">true</public_url></prop></set></propertyupdate>"
-    // "\r\n"
-    // "\r\n"
-    // "\r\n"
-    // "PUT /%s HTTP/1.1\r\n"
-    // "Host: webdav.yandex.ru\r\n"
-    // "Accept: */*\r\n"
-    // "Authorization: OAuth " OAUTH_TOKEN "\r\n"
-    // "Expect: 100-continue\r\n"
-    // "Content-Type: image/png\r\n"
-    // "Content-Length: ",
-
-// "PUT /%s HTTP/1.1\r\n"
-// "Host: webdav.yandex.ru\r\n"
-// "Accept: */*\r\n"
-// "Authorization: OAuth " OAUTH_TOKEN "\r\n"
-// "Expect: 100-continue\r\n"
-// "Content-Type: application/binary\r\n"
-// "Content-Length: ",
-//     // fileName,
-//     // fileName,
-//     fileName
-//   );
-//   int screenshotSizeOffset = requestLength;
-//   requestLength += sprintf(request + requestLength, "          \r\n\r\n");
-//   int screenshotOffset = requestLength;
-//   int screenshotSize = 0;
-//   takeScreenshot(request + requestLength, &screenshotSize);
-//   int screenshotSizeLength = sprintf(request + screenshotSizeOffset, "%d", screenshotSize);
-//   request[screenshotSizeOffset + screenshotSizeLength] = ' ';
-
-//   printf("%s\n", request);
-
-
-
-
-  // log("Writing request");
-  // SSL_write(handle, request, requestLength-screenshotSize);
-  // SSL_write(handle, request+screenshotOffset, screenshotSize);
-
-  // log("Reading response");
-  // static char response[10240];
-  // // while (SSL_read(handle, response, sizeof(response))) {}
-  // SSL_read(handle, response, sizeof(response));
   // printf("\n\n%s", response);
-  // memset(response, 0, sizeof(response));
 
-  // SSL_read(handle, response, sizeof(response));
-  // printf(response);
-  // memset(response, 0, sizeof(response));
+  const char * linkBegin = strstr(response, "https://yadi.sk/");
+  char * linkEnd = strstr(response, "</public_url>");
+  *linkEnd = 0;
+  printf("%s", linkBegin);
+  fflush(stdin);
+  // GtkClipboard * clipboard = gtk_clipboard_get(GDK_SELECTION_PRIMARY);
+  // gtk_clipboard_set_text(clipboard, strdup(linkBegin), strlen(linkBegin));
+  // gtk_clipboard_store(clipboard);
+  // clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
+  // gtk_clipboard_set_text(clipboard, strdup(linkBegin), strlen(linkBegin));
+  // gtk_clipboard_store(clipboard);
 
-  // SSL_read(handle, response, sizeof(response));
-  // printf(response);
-  // memset(response, 0, sizeof(response));
-
-  // SSL_read(handle, response, sizeof(response));
-  // printf(response);
-  // memset(response, 0, sizeof(response));
-
-  // SSL_read(handle, response, sizeof(response));
-  // printf(response);
-  // memset(response, 0, sizeof(response));
-
-  // SSL_read(handle, response, sizeof(response));
-  // printf(response);
-  // memset(response, 0, sizeof(response));
-
-
-
-
-  //   "         \r\n"
-  //   "\r\n",
-
-  //   fileName,
-  //   fileName,
-  //   strlen(BODY), 
-  //   BODY,
-  //   "4.png",
-  //   screenshot.size
-  // );
-
-
-
-
-
-
-
-
-
-
-
-
-  // log("Taking screenshot");
-  // struct Screenshot screenshot = takeScreenshot();
-  // screenshot.data[screenshot.size++] = '\r';
-  // screenshot.data[screenshot.size++] = '\n';
-
-  // log("Sending request");
-  // static char request[1024];
-  // int requestLength = sprintf(request,
-  //   "PUT /%s HTTP/1.1\r\n"
-  //   "Host: webdav.yandex.ru\r\n"
-  //   "Accept: */*\r\n"
-  //   "Authorization: OAuth 45e24fd66c884bafaae7cc4e2e462789\r\n"
-  //   "Expect: 100-continue\r\n"
-  //   "Content-Type: image/png\r\n"
-  //   "Content-Length: 0\r\n"
-  //   "\r\n"
-  //   "PROPPATCH /%s HTTP/1.1\r\n"
-  //   "Host: webdav.yandex.ru\r\n"
-  //   "Authorization: OAuth 45e24fd66c884bafaae7cc4e2e462789\r\n"
-  //   "Content-Length: %lu\r\n"
-  //   "\r\n"
-  //   "%s"
-  //   "\r\n"
-  //   "PUT /%s HTTP/1.1\r\n"
-  //   "Host: webdav.yandex.ru\r\n"
-  //   "Accept: */*\r\n"
-  //   "Authorization: OAuth 45e24fd66c884bafaae7cc4e2e462789\r\n"
-  //   "Expect: 100-continue\r\n"
-  //   "Content-Type: image/png\r\n"
-  //   "Content-Length: %d\r\n"
-  //   "\r\n",
-
-  //   "4.png",
-  //   "4.png",
-  //   strlen(BODY), 
-  //   BODY,
-  //   "4.png",
-  //   screenshot.size
-  // );
-  // SSL_write(handle, request, requestLength);
-  // SSL_write(handle, screenshot.data, screenshot.size);
-
-  // static char response[10240];
-  // // while (SSL_read(handle, response, sizeof(response))) {}
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-
-  // SSL_read(handle, response, sizeof(response));
-  // printf("\n%s\n\n", response);
-
-
-
-  // log("Replacing empty file with screenshot");
-  // requestLength = sprintf(request,
-  // );
-  // SSL_write(handle, request, requestLength);
-
-  // SSL_read(handle, response, sizeof(response));
-
-
-  // SSL_read(handle, response, sizeof(response));
-
-  // requestLength = sprintf(request, ,
-  //   "4.png", strlen(BODY), BODY
-  // );
-
-
-
-  // log("Sending PUT request");
-  // static char request[1024];
-  // int requestLength = sprintf(request,
-  //   "PUT /%s HTTP/1.1\r\n"
-  //   "Host: webdav.yandex.ru\r\n"
-  //   "Accept: */*\r\n"
-  //   "Authorization: OAuth 45e24fd66c884bafaae7cc4e2e462789\r\n"
-  //   "Expect: 100-continue\r\n"
-  //   "Content-Type: image/png\r\n"
-  //   "Content-Length: %d\r\n"
-  //   "\r\n",
-  //   "4.png",
-  //   screenshot.size
-  // );
-  // SSL_write(handle, request, requestLength);
-
-  // // log("Receiving response");
-  // static char response[10240];
-  // // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-
-  // // TODO: check that it is 100 CONTINUE
-  // // printf("|||%u|||\n", strlen("<propertyupdate xmlns=\"DAV:\"><set><prop><public_url xmlns=\"urn:yandex:disk:meta\">true</public_url></prop></set></propertyupdate>"));
-
-  // log("Sending screenshot");
-  // screenshot.data[screenshot.size++] = '\r';
-  // screenshot.data[screenshot.size++] = '\n';
-  // SSL_write(handle, screenshot.data, screenshot.size);
-
-  // // log("Receiving response");
-  // // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-
-
-  // log("Sending publish request");
-  // static const char BODY[] = "<propertyupdate xmlns=\"DAV:\"><set><prop><public_url xmlns=\"urn:yandex:disk:meta\">true</public_url></prop></set></propertyupdate>";
-  // requestLength = sprintf(request, 
-  //   "PROPPATCH /%s HTTP/1.1\r\n"
-  //   "Host: webdav.yandex.ru\r\n"
-  //   "Authorization: OAuth 45e24fd66c884bafaae7cc4e2e462789\r\n"
-  //   "Content-Length: %lu\r\n"
-  //   "\r\n"
-  //   "%s",
-  //   "4.png", strlen(BODY), BODY
-  // );
-  // // puts(request);
-  // SSL_write(handle, request, requestLength);
-
-  // log("Receiving response");
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-  // SSL_read(handle, response, sizeof(response));
-  // // printf("\n%s\n\n", response);
-  // SSL_read(handle, response, sizeof(response));
-  // printf("\n%s\n\n", response);
+  // log("Waiting for requests to complete");
+  SSL_read(handle, response, sizeof(response));
+  SSL_read(handle, response, sizeof(response));
 
   SSL_shutdown(handle);
   close(SSL_get_fd(handle));
 
-  log("Done\n");
+  // log("Done\n");
 
+  // sleep(100);
+
+  // gtk_maset clipboard text cin();
   return 0;
 }
